f0ad016d461aa62cfd63217e47f2aff3faf34984
mohammad <mohammadelkhiat@gmail.com>
2024-12-21 14:57:55 +0200

    added bootstrap styling and, Also added User Authentication with flask-login

--- app.py
+++ app.py
@@ -64,3 +64,65 @@ def edit_exercise(id):
 
     db.session.commit()
     return jsonify({'message': 'Exercise updated!'})
+
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"  # Redirects to login page if not logged in
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:  # Hash passwords in a real app!
+            login_user(user)
+            return redirect(url_for('index'))
+        flash("Invalid credentials!")
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user is None:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Registration successful!")
+            return redirect(url_for('login'))
+        flash("Username already exists!")
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html', user=current_user)
+
+if __name__ == '__main__':
+    db.create_all()  # Creates tables (users.db)
+    app.run(debug=True)
